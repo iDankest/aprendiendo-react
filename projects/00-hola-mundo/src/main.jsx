ed44de09d705beaf98015ffd2c68d9c5cf2a2a14
iDankest <DankestMirror@hotmail.es>
2025-01-22 11:14:17 +0000

    React.Fragment

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,5 +1,10 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<h1>Hello, world!</h1>);
+root.render(
+  <React.Fragment>
+    <h1>Hola Mundo</h1>
+    <button>Este pedazo de boton</button>
+  </React.Fragment>,
+);
