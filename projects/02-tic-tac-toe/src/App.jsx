8a7d5cadab04a36c444adc90ee41d359b7225106
iDankest <DankestMirror@hotmail.es>
2025-01-23 14:49:50 +0000

    Update style

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -5,23 +5,46 @@ const TURNS = {
   O: 'O',
 };
 
-const Square = ({ children, updateBoard, index }) => {
-  return <div className="square">{children}</div>;
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+  const handleClick = () => {
+    updateBoard(index);
+  };
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const updateBoard = (index) => {
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  };
   return (
     <main className="board">
-      <h1>Tic Tac Toe</h1>;
+      <h1>Tic Tac Toe</h1>
       <section className="game">
         {board.map((_, index) => {
-          return <Square key={index} index={index}></Square>;
+          return (
+            <Square
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+            ></Square>
+          );
         })}
       </section>
       <section className="turn">
-        <Square></Square>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   );
